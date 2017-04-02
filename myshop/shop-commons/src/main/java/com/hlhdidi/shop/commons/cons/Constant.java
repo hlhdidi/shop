bd42138f746312492af6a0737e8a05ac8f8eaa55
hlhdidi <2417025525@qq.com>
2017-04-02 17:46:46 +0800

    ok

--- myshop/shop-commons/src/main/java/com/hlhdidi/shop/commons/cons/Constant.java
+++ myshop/shop-commons/src/main/java/com/hlhdidi/shop/commons/cons/Constant.java
@@ -11,6 +11,4 @@ public class Constant {
 	public static final String REDIS_PRODUCTID_KEY="shop_product_id";
 	//购物车的键
 	public static final String REDIS_CART="cart_";
-	//测试Git的hello功能
-    public static final String TEST_asd="hello,hlhdidi";
 }
